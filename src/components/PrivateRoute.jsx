8ef0576f60cf4a074416e13f416ba49b932f5dae
Amina SALMI <asalm@lmdsio.com>
2025-11-13 17:46:31 +0100

    v3

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,9 +2,11 @@ import { useAuth } from "../context/AuthContext"
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useAuth(); 
-
-  if (!user) {
+  const { token, loading } = useAuth(); 
+ if (loading) {
+    return <div>Chargement...</div>;
+  }
+  if (!token) {
     return <Navigate to="/login" />;
   }
 
